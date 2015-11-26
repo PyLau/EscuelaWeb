b942046ffb8bfb4ecef7b16644a0625ff5951570
Laura Parra <lauraparra89@gmail.com>
2015-11-26 17:05:46 -0430

    Making it prettier

--- CursoJS/class05_useless/js/todo.js
+++ CursoJS/class05_useless/js/todo.js
@@ -1,57 +1,42 @@
-//  todo.js
-
-// $ -> jQuery.ready
-
-// 
-//function init() {
-//  console.log('hola, document ready');
-//}
-
-//$(document).ready(init);
-
-var taskCreated = 0;
+// todo.js
 var taskDone = 0;
-
-$(document).ready( function () {
+var taskCreated = 0;
+$(document).ready(function(){
   
-  function refreshCounters () {
+  $('#task-new').focus(); 
+  
+  function refreshCounters(){
     $('#task-count-new').text(taskCreated);
     $('#task-count-done').text(taskDone);
   }
   
-  $('#task-new').focus();
-  
-  $('#task-form').submit( function (evt) {
+  $('#task-form').submit(function(evt){
     evt.preventDefault();
     var taskText = $('#task-new').val();
-    if (taskText === '')
+    if(taskText === '')
       return;
     $('#task-new').val('');
     $('#task-text').text(taskText);
     var taskElement = $('#hidden-item').children();
     $('#task-list').append(taskElement.clone());
-    $('#task-list #task-text').prop('id', '');
+    $('#task-list #task-text').prop('id','');
     taskCreated++;
     refreshCounters();
   });
   
-  $('#task-list').on('change', '.task-check', function (evt) {
+  $('#task-list').on('change','.task-check', function (evt){
     $(this).parent().toggleClass('task-done');
-    if ($(this).parent().hasClass('task-done')) {
+    if($(this).parent().toggleClass('task-done')){
       taskDone++;
-    } else {
+    }else{
       taskDone--;
     }
     refreshCounters();
   });
-      
-  $('#task-list').on('click', '.task-btn', function (evt) {
+    
+  $('#task-list').on('click','.task-btn', function (evt){
     $(this).parent().remove();
     taskCreated--;
     refreshCounters();
-  });
-  
-  
-  
-  
+  });  
 });
--- CursoJS/miniproject/js/miniproject.js
+++ CursoJS/miniproject/js/miniproject.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $('#board-new').focus();
-  
+  //Create board
   $('#board-form').submit(function(evt){
     evt.preventDefault();
     //Creacion de tablero y su titulo
@@ -15,14 +15,19 @@ $(document).ready(function(){
     $('.board-text').editable({type: 'text', title: 'Enter title'});
   });
 
-  //Creacion del task todo dentro del tablero    
+  //Create task inside board
   $('#board-list').on('click','.board-plus-btn', function (evt){
     var taskElement = $('#hidden-item').children();
     $(this).parent().parent().find('#task-list').append(taskElement.clone());
     $('#task-list #task-text').prop('id','');
     $.fn.editable.defaults.mode = 'inline';
-    $('.todo-text').editable();
+    $('.todo-text').editable({value: ""});
   }); 
+
+  // $('#options').on('click','.glyphicon-pencil'), function (evt){
+  //   var optionElement = $('#hidden-options').children();
+  //    $(this).find('#options').append(optionElement.clone());
+  // }); 
 
   $('#task-list').on('click','.task-btn', function (evt){
     $(this).parent().remove();
