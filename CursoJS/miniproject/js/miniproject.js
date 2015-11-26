624fc0b8f817fe4544b3302226073485291184cd
Laura Parra <lauraparra89@gmail.com>
2015-11-25 23:10:49 -0430

    Changes

--- CursoJS/miniproject/js/miniproject.js
+++ CursoJS/miniproject/js/miniproject.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-  $('#board-text').editable();
+
   $('#board-new').focus();
   
   $('#board-form').submit(function(evt){
     evt.preventDefault();
+    //Creacion de tablero y su titulo
     var boardText = $('#board-new').val();
     if(boardText === '')
       return;
@@ -12,16 +13,15 @@ $(document).ready(function(){
     var boardElement = $('#hidden-board').children();
     $('#board-list').append(boardElement.clone());
     $('.board-text').editable({type: 'text', title: 'Enter title'});
-    // $('#board-list #board-text').prop('id','');
   });
 
-  // $(this).parent().remove(); this is for delete
-    
+  //Creacion del task todo dentro del tablero    
   $('#board-list').on('click','.board-plus-btn', function (evt){
     var taskElement = $('#hidden-item').children();
-    // $('#task-list').append(taskElement.clone());
     $(this).parent().parent().find('#task-list').append(taskElement.clone());
     $('#task-list #task-text').prop('id','');
+    $.fn.editable.defaults.mode = 'inline';
+    $('.todo-text').editable();
   }); 
 
   $('#task-list').on('click','.task-btn', function (evt){
